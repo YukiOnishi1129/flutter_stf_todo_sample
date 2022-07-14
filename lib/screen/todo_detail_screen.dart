759a1418040bbeffc10cb2acd2f709ca0a5a5e79
yuki <yuki.onishi1129@gmail.com>
2022-07-15 00:57:53 +0900

    詳細画面のテンプレート作成

--- lib/screen/todo_detail_screen.dart
+++ lib/screen/todo_detail_screen.dart
@@ -1,55 +1,15 @@
 import 'package:flutter/material.dart';
 
-import '../components/layouts/detail_base_body_layout.dart';
+import '../components/templates/todo_detail/todo_detail_template.dart';
 import '../model/todo_model.dart';
 
-class TodoDetailScreen extends StatefulWidget {
+class TodoDetailScreen extends StatelessWidget {
   final Todo todoDetail;
   const TodoDetailScreen({Key? key, required this.todoDetail})
       : super(key: key);
 
   @override
-  State<TodoDetailScreen> createState() => _TodoDetailScreenState();
-}
-
-class _TodoDetailScreenState extends State<TodoDetailScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('詳細'),
-      ),
-      body: DetailBaseBodyLayout(
-        child: Column(
-          children: [
-            TextField(
-              enabled: false, // 非活性
-              controller: TextEditingController(text: widget.todoDetail.title),
-              decoration: const InputDecoration(
-                // https://qiita.com/sekitaka_1214/items/17cccc94a9e7150cdf3a
-                labelText: 'タイトル',
-              ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            TextFormField(
-              enabled: false,
-              // 複数行表示
-              // https://minpro.net/flutter-dart-textfield-fold
-              maxLines: null,
-              controller:
-                  TextEditingController(text: widget.todoDetail.content),
-              decoration: const InputDecoration(
-                labelText: '内容',
-              ),
-            ),
-            const SizedBox(
-              height: 160,
-            )
-          ],
-        ),
-      ),
-    );
+    return TodoDetailTemplate(todoDetail: todoDetail);
   }
 }
