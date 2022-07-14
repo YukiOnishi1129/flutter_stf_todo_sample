e4fc63ca652b759f80a73ed77cc80d879de19192
yuki <yuki.onishi1129@gmail.com>
2022-07-15 01:00:03 +0900

    一覧画面のwidgetをstatelessに変更

--- lib/screen/todo_list_screen.dart
+++ lib/screen/todo_list_screen.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 
 import '../components/templates/todo_list/todo_list_template.dart';
 
-class TodoListScreen extends StatefulWidget {
+class TodoListScreen extends StatelessWidget {
   const TodoListScreen({Key? key}) : super(key: key);
 
-  @override
-  State<TodoListScreen> createState() => _TodoListScreenState();
-}
-
-class _TodoListScreenState extends State<TodoListScreen> {
   @override
   Widget build(BuildContext context) {
     return const TodoListTemplate();
